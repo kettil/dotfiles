39c75aec1bf2b6f7698e86cf0226c51a3f2c92c2
Kettil <development@kettil.de>
2017-10-27 08:01:33 +0200

    add macos description

--- install/macos.sh
+++ install/macos.sh
@@ -3,8 +3,10 @@
 df_install() {
     echo "####################"
     echo "# macos : change settings"
+    echo ""
 
     # change default save location to local storage from iCloud for all Apps
+    echo " - NSDocumentSaveNewDocumentsToCloud"
     defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 }
 
