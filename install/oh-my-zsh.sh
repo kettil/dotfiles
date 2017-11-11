528fd50184f70af611bf8748b6ae49f3d80ce9ee
Kettil <development@kettil.de>
2017-11-11 16:26:22 +0100

    update oh-my-zsh install script

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -18,7 +18,7 @@ df_install() {
     cat "${DF_GITS}/${DF_NAME}/templates/zshrc.zsh-template"                        \
         | sed "s|^export ZSH=.*$|export ZSH=${DF_GITS}/${DF_NAME}|"                 \
         | sed "s|^\(ZSH_THEME=.*\)$|#=\1|"                                          \
-        | sed "s|^\(plugins=.*\)$|#=\1|"                                            \
+        | perl -lpe 'BEGIN{undef $/;} s/^plugins=.*\)/#=plugins=()/smg'             \
         | sed "s|^# \(DISABLE_AUTO_UPDATE.*\)$|\1|"                                 \
         | awk '/DISABLE_AUTO_UPDATE/{print "DISABLE_UPDATE_PROMPT=\"true\""}1'      \
         > "${DF_HOME}/zshrc_oh-my-zsh.sh"
