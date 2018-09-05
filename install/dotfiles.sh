6c3bf69c5df1135bd00631c5beb05aabb8f36ae7
Kjell Dießel <com.github@kettil.de>
2018-09-05 15:59:53 +0200

    Create dotfiles.sh

---
+++ install/dotfiles.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+df_install() {
+    echo "####################"
+    echo "# dotfiles : create symlinks"
+
+    find "$DOTS/configs" -type f |
+        while read line; do
+            DF_FILE="${line/$DOTS\/configs\//}"
+            echo "- ~/${DF_FILE}"
+            ln -sf "$DOTS/configs/${DF_FILE}" ~/"${DF_FILE}"
+        done
+}
+
+case "$1" in
+    install)
+      df_install
+      ;;
+    update)
+      df_install
+      ;;
+    *)
+      echo "Usage: $0 {install|update}"
+esac
+echo ""
