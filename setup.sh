87316ba7c75a40cdafd44354a89f3af6386476d4
Kettil <development@kettil.de>
2018-09-05 23:19:14 +0200

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,41 +6,46 @@ DF_HOME="$(pwd)"
 DF_GITS="${DF_HOME}/gits"
 
 df_run() {
-    mkdir -p "${DF_GITS}"
+  mkdir -p "${DF_GITS}"
 
-    cd "${DF_HOME}"
-    source ./install/dotfiles.sh "${1}"
-    cd "${DF_HOME}"
-    source ./install/os-mac.sh "${1}"
-    cd "${DF_HOME}"
-    source ./install/brew.sh "${1}"
-    cd "${DF_HOME}"
-    source ./install/docker.sh "${1}"
-    cd "${DF_HOME}"
-    source ./install/zshrc.sh "${1}"
-    cd "${DF_HOME}"
-    source ./install/oh-my-zsh.sh "${1}"
-    cd "${DF_HOME}"
-    source ./install/oh-my-zsh-theme-powerlevel9k.sh "${1}"
-    cd "${DF_HOME}"
-    source ./install/oh-my-zsh-plugin.sh "${1}" "zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions.git"
-    cd "${DF_HOME}"
-    source ./install/oh-my-zsh-plugin.sh "${1}" "zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting.git"
+  cd "${DF_HOME}"
+  source ./install/dotfiles.sh "${1}"
+  cd "${DF_HOME}"
+  source ./install/os-mac.sh "${1}"
+  cd "${DF_HOME}"
+  source ./install/brew.sh "${1}"
+  cd "${DF_HOME}"
+  source ./install/docker.sh "${1}"
+  cd "${DF_HOME}"
+  source ./install/zshrc.sh "${1}"
+  cd "${DF_HOME}"
+  source ./install/oh-my-zsh.sh "${1}"
+  cd "${DF_HOME}"
+  source ./install/oh-my-zsh-theme-powerlevel9k.sh "${1}"
+  cd "${DF_HOME}"
+  source ./install/oh-my-zsh-plugin.sh "${1}" "zsh-autosuggestions" "https://github.com/zsh-users/zsh-autosuggestions.git"
+  cd "${DF_HOME}"
+  source ./install/oh-my-zsh-plugin.sh "${1}" "zsh-syntax-highlighting" "https://github.com/zsh-users/zsh-syntax-highlighting.git"
 }
 
 case "$1" in
-    install)
-        df_run "install"
+  install)
+    df_run "install"
 
-        echo "####################"
-        echo "#"
-        echo "# you must call following command if you want to use the zsh shell"
-        echo "# chsh -s $(grep /zsh$ /etc/shells | tail -1)"
-        echo "#"
-        ;;
-    update)
-        df_run "update"
-        ;;
-    *)
-        echo "Usage: $0 {install|update}"
+    echo "####################"
+    echo "#"
+    echo "# you must call following command if you want to use the zsh shell"
+    echo "# chsh -s $(grep /zsh$ /etc/shells | tail -1)"
+    echo "#"
+    ;;
+  update)
+    echo "####################"
+    echo "# update dotfiles repo"
+    git pull
+    echo ""
+
+    df_run "update"
+    ;;
+  *)
+    echo "Usage: $0 {install|update}"
 esac
