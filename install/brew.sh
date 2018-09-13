ba87ced01b34c179ebca862240228d77c2bc4a27
Kjell Dießel <com.github@kettil.de>
2018-09-13 12:10:39 +0200

    Update brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -19,7 +19,7 @@ df_install() {
     df_install_cask osxfuse
     df_install_brew sshfs
     # networking
-    df_install_brew nmap fping
+    df_install_brew nmap fping speedtest-cli
     # monitoring
     df_install_brew htop iftop
     # system tools
@@ -27,14 +27,9 @@ df_install() {
     # runtimes
     df_install_brew node
     # videos
-    df_install_brew ffmpeg mkvtoolnix mp4v2
-    # maybe: libav
-    # youtube
-    df_install_brew youtube-dl atomicparsley
+    df_install_brew ffmpeg mkvtoolnix mp4v2 youtube-dl atomicparsley
     # pdf
     df_install_brew gs qpdf
-    # speed test
-    df_install_brew speedtest-cli
     # check sd cards
     df_install_brew f3
 }
