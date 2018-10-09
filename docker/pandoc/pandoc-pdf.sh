e291cf7940908c69c87ab40d19a7f4a556d042a7
Kettil <development@kettil.de>
2018-10-09 22:02:58 +0200

    update pandoc-pdf.sh

--- docker/pandoc/pandoc-pdf.sh
+++ docker/pandoc/pandoc-pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # generate a hash
-PANDOC_HASH="$(uuid | awk '{print toupper($0)}' | cut -d'-' -f 1,2,3)"
+PANDOC_HASH="$(uuid | awk '{print toupper($0)}' | cut -d'-' -f 2,1,4,3)"
 
 /pandoc.sh --to=latex --variable=hash:"${PANDOC_HASH}" "$@"
