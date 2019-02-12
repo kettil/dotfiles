461d13b7a1d9a265a6a75c7f873a85f394d261ef
Kjell Dießel <com.github@kettil.de>
2019-02-12 19:03:05 +0100

    Update oh-my-zsh-theme-powerlevel9k.sh

--- install/oh-my-zsh-theme-powerlevel9k.sh
+++ install/oh-my-zsh-theme-powerlevel9k.sh
@@ -41,7 +41,7 @@ fi
 
 if [ "$(whoami)" != "$(stat -c %U "${DF_HOME}")" ]; then
     echo "####################"
-    echo "# oh-my-zsh : theme : installation is skipped"
+    echo "# oh-my-zsh : theme : installation is skipped - wrong owner/user"
     echo ""
     return
 fi
