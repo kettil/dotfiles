f314fcdac001eaa2cada728ad04c8fcb127ec7d9
Kjell Dießel <com.github@kettil.de>
2019-02-12 17:24:16 +0100

    Update os-debian.sh

--- install/os-debian.sh
+++ install/os-debian.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
 
 df_install() {
+    echo ""
+    echo "####################"
+    echo "# debian : update"
+    sudo apt-get update
 
+    echo ""
+    echo "####################"
+    echo "# debian : install"
+    sudo apt-get install -y zsh
+    # monitoring
+    sudo apt-get install -y htop iftop
+    # system tools
+    sudo apt-get install -y nano git tree wget
+
+    echo ""
+    echo "####################"
+    echo "# debian : copy fonts"
+    echo ""
+    mkdir -p ~/.local/share/fonts
+    ln -sf ~/.local/share/fonts ~/.fonts
+    cp $DF_HOME/files/*.ttf ~/.fonts
+    chmod -R 644 ~/.fonts/*
 }
 
 if [ ! -f /etc/debian_version ]; then
