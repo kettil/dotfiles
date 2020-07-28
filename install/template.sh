e1a54f77336e4d985ede5c70a37013b14792b7aa
Kettil <development@kettil.de>
2020-07-28 17:08:50 +0200

    refactor(install): move .env file into home directory

--- install/template.sh
+++ install/template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DF_TEMPLATE_ENV="$DF_HOME/.env"
+DF_TEMPLATE_ENV="~/.dotfiles_env"
 DF_TEMPLATE_ENVTXT="$DF_TEMPLATE_ENV.txt"
 DF_TEMPLATE_SOURCE="$DF_HOME/templates"
 DF_TEMPLATE_TARGET="$DF_HOME/configs"
