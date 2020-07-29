83d76a319179e1e6152ede04cb1c00aadbdeb4e7
Kjell Dießel <development@kettil.de>
2020-07-29 16:29:10 +0200

    feat: add time measurement

--- brew-bin/flash-check.sh
+++ brew-bin/flash-check.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-echo "#"
-echo "# Start SD card check"
-echo "#####################"
-echo "#"
-echo "# time: $(date)"
-echo "# start write test"
-echo "#"
+echo "#########################"
+echo "#  Start SD card check  #"
+echo "#########################"
+echo ""
+timeWriteStart=$(date +%s)
 
 /usr/local/bin/f3write "${1}"
 
-echo "#"
-echo "# end write test"
-echo "# time: $(date)"
-echo "# start read test"
-echo "#"
+timeWriteEnd=$(date +%s)
+echo '#'
+echo "# Elapsed time for \"write\": $((timeWriteEnd - $timeWriteStart))s"
+echo '#'
+echo ""
+timeReadStart=$(date +%s)
 
 /usr/local/bin/f3read "${1}"
 
-echo "#"
-echo "# end write test"
-echo "# time: $(date)"
-echo "#"
+timeReadEnd=$(date +%s)
+echo '#'
+echo "# Elapsed time for \"write\": $((timeWriteEnd - $timeWriteStart))s"
+echo "# Elapsed time for \"read\":  $((timeReadEnd - $timeReadStart))s"
+echo "# Elapsed time:             $((timeReadEnd - $timeWriteStart))s"
+echo '#'
+echo ""
