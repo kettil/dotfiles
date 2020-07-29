889787ea25c0a609e0f57dc9fa111280da85cca4
Kjell Dießel <development@kettil.de>
2020-07-29 16:14:09 +0200

    refactor: improved readability

--- brew-bin/pdf-merged.sh
+++ brew-bin/pdf-merged.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dAutoRotatePages=/None -dPDFSETTINGS=/default -dNOPAUSE -dQUIET -dBATCH -dDetectDuplicateImages -dCompressFonts=true -r150 -sOutputFile=merged-$(date +%Y-%m-%d-%H-%M-%S).pdf "$@"
+gs -sDEVICE=pdfwrite                                  \
+  -dCompatibilityLevel=1.4                            \
+  -dAutoRotatePages=/None                             \
+  -dPDFSETTINGS=/default                              \
+  -dNOPAUSE                                           \
+  -dQUIET                                             \
+  -dBATCH                                             \
+  -dDetectDuplicateImages                             \
+  -dCompressFonts=true                                \
+  -r150                                               \
+  -sOutputFile=merged-$(date +%Y-%m-%d-%H-%M-%S).pdf  \
+  "$@"
--- brew-bin/pdf-smaller.sh
+++ brew-bin/pdf-smaller.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/ebook -dNOPAUSE -dQUIET -dBATCH -sOutputFile=${1%\.pdf}-small.pdf $1
+gs -sDEVICE=pdfwrite                    \
+  -dCompatibilityLevel=1.4              \
+  -dPDFSETTINGS=/ebook                  \
+  -dNOPAUSE                             \
+  -dQUIET                               \
+  -dBATCH                               \
+  -sOutputFile=${1%\.pdf}-small.pdf     \
+  $1
