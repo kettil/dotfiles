664dc8520a5ea515fa7fb2af7f7393af133913cb
Kjell Dießel <development@kettil.de>
2021-03-23 11:26:17 +0100

    feat(mac): add new macos default settings

--- install/os-mac.sh
+++ install/os-mac.sh
@@ -22,9 +22,32 @@ echo "####################"
 echo "# macos : change settings"
 echo ""
 
-# change default save location to local storage from iCloud for all Apps
+# Always show scrollbars
+echo "- AppleShowScrollBars"
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+# Possible values: `WhenScrolling`, `Automatic` and `Always`
+
+# Expand save panel by default
+echo "- NSNavPanelExpandedStateForSaveMode"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expand print panel by default
+echo "- PMPrintingExpandedStateForPrint"
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Change default save location to local storage from iCloud for all Apps
 echo "- NSDocumentSaveNewDocumentsToCloud"
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Automatically quit printer app once the print jobs complete
+echo "- PrintingPrefs - Quit When Finished"
+defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
+
+# Enable “natural” (Lion-style) scrolling
+echo "- swipescrolldirection"
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool true
 
 echo ""
 echo "####################"
