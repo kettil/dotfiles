83479ca87ceec30ae4b028cd8bcece2bf14e6edd
Kjell Dießel <development@kettil.de>
2021-03-23 10:56:27 +0100

    feat(zsh): add alias

--- templates/zshrc.sh
+++ templates/zshrc.sh
@@ -204,12 +204,33 @@ fi
 
 source $DOTS/zshrc_oh-my-zsh.sh
 
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+	colorflag="--color"
+	export LS_COLORS='no=00:fi=00:di=01;31:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
+else # macOS `ls`
+	colorflag="-G"
+	export LSCOLORS='BxBxhxDxfxhxhxhxhxcxcx'
+fi
+
 #
 # alias list
 #
 
-alias ll='/bin/ls -lisah'
+alias ls='/bin/ls -F ${colorflag}'
+alias ll='/bin/ls -lisahF ${colorflag}'
 alias nano='/usr/local/bin/nano -lc'
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Always enable colored `grep` output
+# Note: `GREP_OPTIONS="--color=auto"` is deprecated, hence the alias usage.
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
 
 # creates a folder and goes into it
 mkcdir() { mkdir -p -- "$1" &&  cd -P -- "$1"; }
@@ -236,9 +257,24 @@ if [ "${OSTYPE:0:6}" = "darwin" ]; then
     alias ios='open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app'
     alias fileMerge='open /Applications/Xcode.app/Contents/Applications/FileMerge.app'
 
+    # Show/hide hidden files in Finder
+    alias showDotfiles="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+    alias hideDotfiles="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+    # Hide/show all desktop icons (useful when presenting)
+    alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+    alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
     if [ -e "/Applications/Visual Studio Code.app" ]; then
         # Add Visual Studio Code (code)
         ln -sf /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code $DOTS/bin/code
+    fi
+    if [ -e "/Applications/Google\ Chrome.app" ]; then
+        alias chrome='/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
+    fi
+    if [ -e "/Applications/Google\ Chrome\ Canary.app" ]; then
+        # Add Visual Studio Code (code)
+        alias canary='/Applications/Google\ Chrome\ Canary.app/Contents/MacOS/Google\ Chrome\ Canary'
     fi
 fi
 
